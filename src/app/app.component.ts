d0610b5eefb582df118a11196a88abb46844869e
eximus <fabio4335@gmail.com>
2022-04-06 16:28:22 -0400

    funky text animations and icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+interface Artist {
+  name: string;
+  url: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'milk-moon';
+  artists: Artist[] = [
+    { name: "Danielle ♥ ", url: "" },
+    { name: "Max 📸", url: "" },
+    { name: "Joe 🌞 ", url: "" },
+    { name: "Aspirin 🐈 ", url: "" },
+    { name: "Other", url: "" },
+    { name: "Other", url: "" },
+    { name: "Other", url: "" },
+    { name: "Other", url: "" },
+    { name: "Other", url: "" },
+    { name: "Other", url: "" },
+    { name: "Other", url: "" },
+    { name: "Other", url: "" },
+    { name: "Other", url: "" },
+  ]
 }
