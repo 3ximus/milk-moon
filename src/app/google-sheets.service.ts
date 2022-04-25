efcf75c8a673e2fa8c77e1d64e7f6594c64a1e21
eximus <fabio4335@gmail.com>
2022-04-25 16:01:23 -0400

    final working update

--- src/app/google-sheets.service.ts
+++ src/app/google-sheets.service.ts
@@ -23,33 +23,33 @@ export class GoogleSheetsService {
   getSheetData(): Observable<Artist[]> {
     const sheetid = '1c-dJjel3KBgQ-Bg9vxAAhGCLkiFba9Ijud1OzAmhyzo';
 
-    return new Observable<Artist[]>((subscriber) => {
-      subscriber.next([
-        { name: 'Danielle Romaine', link: '', paid: 'x' },
-        { name: 'Joe Carlo Romero', link: '', paid: 'x' },
-        { name: 'Maxime Jarzynski', link: '', paid: 'x' },
-        { name: 'Riad El Mahmoudy', link: '', paid: 'x' },
-        { name: 'H-L. Lapointe-Guevara', link: '', paid: 'x' },
-        { name: 'Ryann Goldd', link: '', paid: 'x' },
-        { name: 'Edzin', link: '', paid: 'x' },
-        { name: 'Karine Bazerghi', link: '', paid: 'x' },
-        { name: 'Emma Ruixin Tian', link: '', paid: 'x' },
-        { name: 'Eugenie Artoun', link: '', paid: 'x' },
-        { name: 'Nel Green', link: '', paid: 'x' },
-        { name: 'Riesbri', link: '', paid: 'x' },
-        { name: 'Crawford Bennett-Diaz', link: '', paid: 'x' },
-        { name: 'Milla Lopes', link: '', paid: 'x' },
-        { name: 'Paige', link: '', paid: 'x' },
-        { name: 'M. Menou-Maillet', link: '', paid: 'x' },
-        { name: 'Niss Ink', link: '', paid: 'x' },
-      ]);
-      subscriber.complete();
-    });
+    // return new Observable<Artist[]>((subscriber) => {
+    //   subscriber.next([
+    //     { name: 'Danielle Romaine', link: '', paid: 'x' },
+    //     { name: 'Joe Carlo Romero', link: '', paid: 'x' },
+    //     { name: 'Maxime Jarzynski', link: '', paid: 'x' },
+    //     { name: 'Riad El Mahmoudy', link: '', paid: 'x' },
+    //     { name: 'H-L. Lapointe-Guevara', link: '', paid: 'x' },
+    //     { name: 'Ryann Goldd', link: '', paid: 'x' },
+    //     { name: 'Edzin', link: '', paid: 'x' },
+    //     { name: 'Karine Bazerghi', link: '', paid: 'x' },
+    //     { name: 'Emma Ruixin Tian', link: '', paid: 'x' },
+    //     { name: 'Eugenie Artoun', link: '', paid: 'x' },
+    //     { name: 'Nel Green', link: '', paid: 'x' },
+    //     { name: 'Riesbri', link: '', paid: 'x' },
+    //     { name: 'Crawford Bennett-Diaz', link: '', paid: 'x' },
+    //     { name: 'Milla Lopes', link: '', paid: 'x' },
+    //     { name: 'Paige', link: '', paid: 'x' },
+    //     { name: 'M. Menou-Maillet', link: '', paid: 'x' },
+    //     { name: 'Niss Ink', link: '', paid: 'x' },
+    //   ]);
+    //   subscriber.complete();
+    // });
 
-    // return this.googleSheetsDbService.get<Artist>(
-    //   sheetid,
-    //   'Artists',
-    //   artistsMap
-    // );
+    return this.googleSheetsDbService.get<Artist>(
+      sheetid,
+      'Artists',
+      artistsMap
+    );
   }
 }
